f0f7fa2415a5de2692130ee081d76f86ee78d942
DomiasPL <dominik-stepuch@wp.pl>
2019-08-23 16:51:44 +0200

    Start of the project. Added necessary dependencies. Created home controller and home jsp to test tomcat server

---
+++ src/main/java/com/github/dominaspl/pokemonrestapi/PokemonRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.dominaspl.pokemonrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonRestApiApplication.class, args);
+    }
+
+}
---
+++ src/main/webapp/views/home.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dominik
+  Date: 8/23/19
+  Time: 4:44 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page isELIgnored="false" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+
+    <h1>This is home page</h1>
+
+</body>
+</html>
